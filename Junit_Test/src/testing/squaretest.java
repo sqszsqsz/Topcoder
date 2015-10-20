47e159cdc6b3f3f2e6f268e22bc1eef70e1de25f
sqszsqsz <sqszsqsz@hotmail.com>
2015-10-20 16:58:24 -0500

    Added Junit_Test

---
+++ Junit_Test/src/testing/squaretest.java
@@ -0,0 +1,30 @@
+package testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class squaretest {
+
+	@Test
+	public void test() {
+		Junit_testing test1=new Junit_testing();
+		int output=test1.square(5);
+		assertEquals(output,25);
+	}
+	
+	@Test
+	public void test1() {
+		Junit_testing test1=new Junit_testing();
+		int output=test1.square(6);
+		assertEquals(output,36);
+	}
+	
+	@Test
+	public void test2() {
+		Junit_testing test1=new Junit_testing();
+		int output=test1.countA("ABCDABCD");
+		assertEquals(output,2);
+	}
+
+}
